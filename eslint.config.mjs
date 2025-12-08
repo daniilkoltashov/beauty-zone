af764d0755bc6c1e47d790eb4cd474d102ac19c9
Qnaxxi <koltashovdaniil@gmail.com>
2025-12-08 14:27:10 +0200

    update

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+    },
+    languageOptions: { globals: globals.browser },
+  },
+])
